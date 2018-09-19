323865020250e284b62f5a9722081c8c9ffe16fa
L.Chen <1570184051@qq.com>
2018-09-19 17:08:05 +0800

    change

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     keywords=["fractal", "分形"],
     description="对分形比较感兴趣，看pypi上没有相关库，自己撸着玩",
     license="MIT",
-    author="陈粮",
+    author="L.Chen",
     author_email="1570184051@qq.com",
     packages=find_packages(),
     install_requires=["pygame>=1.9.3"],
     platforms="any",
-    url="https://github.com/ChenL1994/fractal",
+    url="https://github.com/pysrc/fractal",
     zip_safe=False,
     package_data={
         "": ["*.dll", "*.pyd", "*.so"]
